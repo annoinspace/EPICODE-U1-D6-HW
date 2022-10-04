65173976e7a0737a20790c42edf3150877c1aee4
annoinspace <aneesahalmask@gmail.com>
2022-10-04 12:34:59 +0100

    html and js files linked
    basic information added to both

---
+++ index.js
@@ -0,0 +1,12 @@
+let myName = `Aneesah`
+let middleName = `Almas`
+let lastName = `Khan`
+
+let myFullName = myName + ` ` + middleName + ` ` + lastName
+console.log(`Hello, my name is ` + myFullName)
+
+let myAge = 23
+let currentYear = 2022
+
+let birthYear = currentYear - myAge
+console.log(`My birth year is ` + birthYear)
